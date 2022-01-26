50779cbffba1e96dcc7dec15bdaf482e784c79e7
muunioi <muunii06@gmail.com>
2022-01-26 20:40:44 +0900

    등록 API

---
+++ src/main/java/com/muunioi/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.muunioi.book.springboot.service.posts;
+
+import com.muunioi.book.springboot.domain.posts.PostsRepository;
+import com.muunioi.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
