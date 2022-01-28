a808fc33b524225e3acde18ea1f2529bbea1202c
muunioi <muunii06@gmail.com>
2022-01-28 22:08:02 +0900

    JPA Auditing

---
+++ src/main/java/com/muunioi/book/springboot/domain/posts/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package com.muunioi.book.springboot.domain.posts;
+
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass //JPA Entity 클래스가 BaseTimeEntity 를 상속할 경우 필드(created/modifiedDate)들도 칼럼으로 인식하도록 함
+@EntityListeners(AuditingEntityListener.class) //BaseTimeEntity 클래스에 Auditing 기능 포함
+public class BaseTimeEntity {
+    // 모든 Entity 의 상위 클래스가 되어 Entity 들의 create/modified Date 관리
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
--- src/test/java/com/muunioi/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/muunioi/book/springboot/web/PostsApiControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 //왜 자동 import가 안되는지 찾아보기
@@ -57,6 +58,7 @@ public class PostsApiControllerTest {
         ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
 
         //then
+        //Junit.assertThat 보다 assertj.assertThat 사용 추천
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).isGreaterThan(0L);
         List<Posts> all = postsRepository.findAll();
@@ -84,19 +86,47 @@ public class PostsApiControllerTest {
 
         String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
 
+        //HTTP 요청 또는 응답에 해당하는 클래스, HttpHeader 와 HttpBody 를 포함
         HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
 
         //when
+        //HttpEntity 클래스를 상속받아 구현한 클래스 -> RequestEntity, ResponseEntity
+        //ResponseEntity 는 HttpRequest 에 대한 응답을 포함하는 클래스 = HttpStatus, HttpHeader, HttpBody 를 포함함함
         ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
 
         //then
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
 
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+
+        //given
+        LocalDateTime now = LocalDateTime.of(2022,1,28,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>> createDate="
+                            +posts.getCreateDate()
+                            +", modifiedDate="
+                            +posts.getModifiedDate());
+
+        assertThat(posts.getCreateDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
 }
