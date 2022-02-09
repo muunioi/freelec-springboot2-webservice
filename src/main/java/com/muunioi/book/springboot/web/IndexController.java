fb5d9338570bcb6cef478a062a553a1a0fd0dea3
muunioi <muunii06@gmail.com>
2022-02-09 15:58:59 +0900

    Mustache_수정/삭제

--- src/main/java/com/muunioi/book/springboot/web/IndexController.java
+++ src/main/java/com/muunioi/book/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.muunioi.book.springboot.web;
 
 import com.muunioi.book.springboot.service.posts.PostsService;
+import com.muunioi.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -24,5 +26,13 @@ public class IndexController {
         return "posts-save";
     }
 
+    //업데이트
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
 
 }
